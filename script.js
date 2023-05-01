6d985c2300266218507b67a049e88ff2f04074ff
Anthony Elison <tonyelison37@gmail.com>
2023-05-01 15:44:29 -0600

    Implement turn system, prevent clicking marked cells

--- script.js
+++ script.js
@@ -1,56 +1,64 @@
 const gameBoard = (() => {
   let array = [];
-  const boardElement = document.querySelector('.game-board');
 
   const getArray = () => array;
 
-  const addMark = (gridCell, index) => {
-    gridCell.textContent = 'X';
+  const addMark = (gridCell, index, symbol) => {
+    gridCell.textContent = symbol;
     gridCell.classList.add('marked');
-    array[index] = 'X';
-  };
-
-  const render = () => {
-    for (let i = 0; i < 9; i += 1) {
-      const gridCell = document.createElement('div');
-      gridCell.id = i;
-      gridCell.addEventListener('click', () => addMark(gridCell, i));
-      boardElement.appendChild(gridCell);
-    }
-  };
-
-  const checkForWinner = () => {
+    array[index] = symbol;
   };
 
   const reset = () => {
     array = [];
   };
 
-  return {
-    getArray, render, checkForWinner, reset,
-  };
+  return { getArray, reset, addMark };
 })();
 
 const playerFactory = (symbol) => {
-  let score = 0;
-
-  const incrementScore = () => {
-    score += 1;
-  };
-
   const getSymbol = () => symbol;
 
-  return { getSymbol, incrementScore };
+  return { getSymbol };
 };
 
 const game = ((board) => {
   const player1 = playerFactory('X');
   const player2 = playerFactory('O');
 
+  let activePlayer = player1;
+
+  const setActivePlayer = () => {
+    activePlayer = activePlayer === player1 ? player2 : player1;
+  };
+
+  const checkForWinner = () => {
+    board.getArray();
+  };
+
+  const playTurn = (gridCell, markIndex) => {
+    if (!board.getArray()[markIndex]) {
+      board.addMark(gridCell, markIndex, activePlayer.getSymbol());
+      setActivePlayer();
+      checkForWinner();
+    }
+  };
+
+  const render = () => {
+    const boardElement = document.querySelector('.game-board');
+    for (let i = 0; i < 9; i += 1) {
+      const gridCell = document.createElement('div');
+      gridCell.id = i;
+      gridCell.addEventListener('click', () => playTurn(gridCell, i));
+      boardElement.appendChild(gridCell);
+    }
+  };
+
   const play = () => {
+    render();
   };
 
   return { play };
 })(gameBoard);
 
-gameBoard.render();
+game.play();
